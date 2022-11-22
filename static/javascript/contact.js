20cbe089985072142e98e47657d7232bad364a92
Liam <ltags230@gmail.com>
2022-11-21 21:40:33 -0600

    slight change

--- static/javascript/contact.js
+++ static/javascript/contact.js
@@ -1,6 +1,4 @@
 
-emailjs.init('3p-bi8k1ve2ape4R7')
-
 window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();

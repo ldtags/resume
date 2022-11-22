02b8b6ab93588349786725de1be65d044960b499
Liam <ltags230@gmail.com>
2022-11-21 19:19:34 -0600

    small changes

--- static/javascript/contact.js
+++ static/javascript/contact.js
@@ -1,61 +1,17 @@
 
-var form_id_js = "contact-form";
+emailjs.init('3p-bi8k1ve2ape4R7')
 
-var data_js = {
-    "access_token": "0dc241fglossr33qk3xtc2ef"
-};
-
-function js_onSuccess() {
-    // remove this to avoid redirect
-    window.location = window.location.pathname + "?message=Email+Successfully+Sent%21&isError=0";
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
 }
-
-function js_onError(error) {
-    // remove this to avoid redirect
-    window.location = window.location.pathname + "?message=Email+could+not+be+sent.&isError=1";
-}
-
-var sendButton = document.getElementById("js_send");
-
-function js_send() {
-    sendButton.value='Sending…';
-    sendButton.disabled=true;
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if (request.readyState == 4 && request.status == 200) {
-            js_onSuccess();
-        } else
-        if(request.readyState == 4) {
-            js_onError(request.response);
-        }
-    };
-
-    var subject = document.querySelector("#" + form_id_js + " [name='subject']").value;
-    var message = document.querySelector("#" + form_id_js + " [name='text']").value;
-    data_js['subject'] = subject;
-    data_js['text'] = message;
-    var params = toParams(data_js);
-
-    request.open("POST", "https://postmail.invotes.com/send", true);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    request.send(params);
-
-    return false;
-}
-
-sendButton.onclick = js_send;
-
-function toParams(data_js) {
-    var form_data = [];
-    for ( var key in data_js ) {
-        form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
-    }
-
-    return form_data.join("&");
-}
-
-var js_form = document.getElementById(form_id_js);
-js_form.addEventListener("submit", function (e) {
-    e.preventDefault();
-});
